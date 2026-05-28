b9483226742069570ac5c2d80d3175cdfc1303ec
kk0506014 <kk0506014@gmail.com>
2026-05-28 11:40:10 +0900

    feat: 리뷰 텍스트 전처리 함수 추가

--- review_collector.py
+++ review_collector.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import re
 
 url = "https://store.steampowered.com/appreviews/413150"
 
@@ -11,6 +12,32 @@ params = {
 
 reviews_list = []
 cursor = "*"
+
+def clean_text(text):
+    # HTML 태그 제거
+    text = re.sub(r"<.*?>", "", text)
+
+    # 특수문자 제거
+    text = re.sub(r"[^\w\s가-힣]", "", text)
+
+    # 소문자 변환
+    text = text.lower()
+
+    # 공백 정리
+    text = re.sub(r"\s+", " ", text).strip()
+
+    return text
+
+sample_reviews = [
+    "<b>정말 재미있어요!!</b>",
+    "농장 게임 중 최고입니다ㅎㅎㅎㅎ",
+    "GOOD GAME!!!"
+]
+
+for review in sample_reviews:
+    print("원본:", review)
+    print("전처리:", clean_text(review))
+    print("-" * 50)
 
 while len(reviews_list) < 1000:
 
